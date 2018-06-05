4177cee8f534b0ed59deff6347dbe9e060b211e0
Lorena <lorena.giddens@outlook.com>
2018-06-05 01:26:52 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
-# def square_array(array)
-#   new_array = []
-#   array.each{ |x| new_array << x ** 2 }
-#   new_array
-# end
-
-def square_array(num)
-  num.collect { |x| x*x }
+def square_array(array)
+  new_array = []
+  array.each{ |x| new_array << x ** 2 }
+  new_array
 end
